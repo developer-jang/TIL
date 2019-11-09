391742054dcee0056dbbf91ec3c42e7c6c1dc156
장준환/플랫폼개발팀 <joonhwan.jang@musinsa.com>
2019-11-09 23:29:39 +0900

    initial commit

---
+++ src/test/java/com/developerjang/til/TilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developerjang.til;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
